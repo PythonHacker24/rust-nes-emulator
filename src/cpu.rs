5a0b94253903c5ba8c74e8446662953cb3af4759
PythonHacker24 <adityapatil24680@gmail.com>
2024-11-05 16:12:04 +0530

    Implemented memory addressing and support for uploading code into memory. Now it adheres to Von Neuman architecutre and is ready to execute code. Story: Mistakenly wrote 0x80000 instead of 0x8000 in load method causing code to load in different part of memory which lead to terminating program without error since 0x00 was loaded. No errors and test cases faild.

--- src/cpu.rs
+++ src/cpu.rs
@@ -74,32 +74,6 @@ impl CPU {
             memory: [0; 0xFFFF] // Program ROM
         }
     }
-
-    // Read data from given memory location
-    fn mem_read(&self, addr: u16) -> u8 {
-        self.memory[addr as usize]
-    }
-
-    // Write given data into given memory location
-    fn mem_write(&mut self, addr: u16, data: u8) {
-        self.memory[addr as usize] = data
-    }
-    
-    // Read 16 bit address from memory and interpret little endian
-    fn mem_read_u16(&mut self, pos: u16) -> u16 {
-        let lo = self.mem_read(pos) as u16;         // lower
-        let hi = self.mem_read(pos + 1) as u16;     // higher
-        (hi << 8) | (lo as u16)                     // ho00000000 + 00000000lo
-    }
-    
-    // Write 16 bit address to memory in little endian
-    fn mem_write_u16(&mut self, pos: u16, data: u16) {
-        let hi = (data >> 8) as u8;
-        let lo = (data & 0xff) as u8; 
-
-        self.mem_write(pos, lo);
-        self.mem_write(pos + 1, hi)
-    }
     
     // Restore state of all registers and initialize program_counter by 2 byte value stored at
     // 0xFFFC (default place where NES CPU stores the value of program counter when cartrige is
@@ -115,7 +89,7 @@ impl CPU {
 
     // Load instructions from a Vector and place them in correct memory locations 
     pub fn load(&mut self, program: Vec<u8>) {
-        self.memory[0x80000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
+        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]);
         self.mem_write_u16(0xFFFC, 0x8000)
     }
 
@@ -133,6 +107,12 @@ impl CPU {
 
         self.register_a = value;
         self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    // Store the value of accumulator in given memory location
+    fn sta(&mut self, mode: &AddressingMode) {
+        let addr = self.get_operand_address(&mode);
+        self.mem_write(addr, self.register_a)
     }
  
     // Function for 0xAA Opscode - Transfer value of A to X
@@ -235,17 +215,27 @@ impl CPU {
     pub fn run(&mut self) {
         // Looping through all instructions
         
+        let ref opcodes: HashMap<u8, &'static opcodes::OpCode> = *opcodes::OPCODES_MAP;    
+
         // Program counter is initialized in load() with value 0x8000
         loop {
 
             // Opscode would be read from memory
             let code = self.mem_read(self.program_counter);
             self.program_counter += 1;
+            let program_counter_state = self.program_counter; 
+
+            let opcode = opcodes.get(&code).expect(&format!("OpCode {:x} is not recognized", code));
 
             match code {
-                0xA9 => {
-                    self.lda(&AddressingMode::Immediate);
-                    self.program_counter += 1;
+                // LDA - Load Data Accumulator
+                0xa9 | 0xa5 | 0xb5 | 0xad | 0xbd | 0xb9 | 0xa1 | 0xb1 => {
+                    self.lda(&opcode.mode);
+                }
+                
+                // STA - Store Accumulator Data
+                0x85 | 0x95 | 0x8d | 0x9d | 0x99 | 0x81 | 0x91 => {
+                    self.sta(&opcode.mode);
                 }
 
                 0xAA => self.tax(),
@@ -255,6 +245,10 @@ impl CPU {
                 0x00 => return,
                 
                 _ => todo!(),
+            }
+
+            if program_counter_state == self.program_counter {
+                self.program_counter += (opcode.len - 1) as u16;
             }
         }
     }
